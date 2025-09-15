ef1a691fc91c3b497e67e084feeae735a68527a0
Nikita Shulga <nikita.shulga@gmail.com>
2025-09-15 14:13:29 -0700

    [CI] Move Windows build/tests to Python-3.10
    ghstack-source-id: b563ba62f632e057e8fff38fe0ec3369a2d063f5
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/162862

--- .ci/pytorch/win-test-helpers/installation-helpers/activate_miniconda3.bat
+++ .ci/pytorch/win-test-helpers/installation-helpers/activate_miniconda3.bat
@@ -3,24 +3,37 @@ if "%BUILD_ENVIRONMENT%"=="" (
 ) else (
   set CONDA_PARENT_DIR=C:\Jenkins
 )
+set CONDA_ROOT_DIR=%CONDA_PARENT_DIR%\Miniconda3
 
+echo "CD is %CD%"
 
 :: Be conservative here when rolling out the new AMI with conda. This will try
 :: to install conda as before if it couldn't find the conda installation. This
 :: can be removed eventually after we gain enough confidence in the AMI
-if not exist %CONDA_PARENT_DIR%\Miniconda3 (
+if not exist %CONDA_ROOT_DIR% (
   set INSTALL_FRESH_CONDA=1
 )
+
+echo "Install fresh conda is %INSTALL_FRESH_CONDA%"
 
 if "%INSTALL_FRESH_CONDA%"=="1" (
   curl --retry 3 --retry-all-errors -k https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe --output %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe
   if errorlevel 1 exit /b
   if not errorlevel 0 exit /b
 
-  %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /AddToPath=0 /D=%CONDA_PARENT_DIR%\Miniconda3
+  %TMP_DIR_WIN%\Miniconda3-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /AddToPath=0 /D=%CONDA_ROOT_DIR%
   if errorlevel 1 exit /b
   if not errorlevel 0 exit /b
 )
 
+echo "Before activate"
 :: Activate conda so that we can use its commands, i.e. conda, python, pip
-call %CONDA_PARENT_DIR%\Miniconda3\Scripts\activate.bat %CONDA_PARENT_DIR%\Miniconda3
+call %CONDA_ROOT_DIR%\Scripts\activate.bat %CONDA_ROOT_DIR%
+echo "After activate, before env create"
+
+echo on
+conda create -y -n py_tmp python=%PYTHON_VERSION%
+echo "After env create, before pip install"
+conda run -n py_tmp pip install -r requirements.txt
+set PATH="%CONDA_ROOT_DIR%\envs\py_tmp;%PATH%
+echo "PATH=%PATH%"
